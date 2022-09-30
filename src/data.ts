805ca8b630715259d44b206e26cf347a83921208
Pedro Sanchez <pedro13jzc@gmail.com>
2022-09-30 16:55:46 -0400

    ci: eslint

--- src/data.ts
+++ src/data.ts
@@ -2,99 +2,99 @@ import { format } from "date-fns";
 
 export type Game = {
   score: {
-    [team:string]: number | null
-  }
-  date: string
-}
+    [team: string]: number | null;
+  };
+  date: string;
+};
 
-export const data:Game[] = [
+export const data: Game[] = [
   {
     score: {
       "Manchester United": 1,
-      "Leicester City": 2
+      "Leicester City": 2,
     },
-    date: "2021-05-04T14:00:00"
+    date: "2021-05-04T14:00:00",
   },
   {
     score: { "Manchester United": null, Liverpool: null },
-    date: "2021-05-09T11:00:00"
+    date: "2021-05-09T11:00:00",
   },
   {
     score: { "Manchester United": 1, "Tottenham Hotspur": 1 },
-    date: "2021-05-05T11:00:00"
+    date: "2021-05-05T11:00:00",
   },
   {
     score: { "Manchester United": 2, Chelsea: 2 },
-    date: "2021-03-04T17:00:00"
+    date: "2021-03-04T17:00:00",
   },
   {
     score: { "Manchester United": 1, "Manchester City": 2 },
-    date: "2021-03-05T17:00:00"
+    date: "2021-03-05T17:00:00",
   },
   {
     score: { "Manchester United": 6, Arsenal: 2 },
-    date: "2021-01-04T17:00:00"
+    date: "2021-01-04T17:00:00",
   },
   {
     score: { Liverpool: null, "Tottenham Hotspur": null },
-    date: "2021-05-09T17:00:00"
+    date: "2021-05-09T17:00:00",
   },
   {
     score: { Liverpool: null, "Leicester City": null },
-    date: "2021-05-06T17:00:00"
+    date: "2021-05-06T17:00:00",
   },
   {
     score: { "Manchester City": 1, Liverpool: 2 },
-    date: "2021-05-01T14:00:00"
+    date: "2021-05-01T14:00:00",
   },
   {
     score: { Arsenal: 2, Liverpool: 3 },
-    date: "2021-05-02T17:00:00"
+    date: "2021-05-02T17:00:00",
   },
   {
     score: { Chelsea: 1, Liverpool: 3 },
-    date: "2021-04-29T17:00:00"
+    date: "2021-04-29T17:00:00",
   },
   {
     score: { "Manchester City": 0, Chelsea: 0 },
-    date: "2021-04-16T17:00:00"
+    date: "2021-04-16T17:00:00",
   },
   {
     score: { "Manchester City": 1, "Leicester City": 2 },
-    date: "2021-04-17T17:00:00"
+    date: "2021-04-17T17:00:00",
   },
   {
     score: { "Manchester City": null, "Tottenham Hotspur": null },
-    date: "2021-05-06T12:00:00"
+    date: "2021-05-06T12:00:00",
   },
   {
     score: { "Manchester City": null, Arsenal: null },
-    date: "2021-05-11T14:00:00"
+    date: "2021-05-11T14:00:00",
   },
   {
     score: { Arsenal: null, Chelsea: null },
-    date: "2021-05-11T12:00:00"
+    date: "2021-05-11T12:00:00",
   },
   {
     score: { Arsenal: 0, "Leicester City": 0 },
-    date: "2021-04-28T12:00:00"
+    date: "2021-04-28T12:00:00",
   },
   {
     score: { Arsenal: 1, "Tottenham Hotspur": 1 },
-    date: "2021-05-04T12:00:00"
+    date: "2021-05-04T12:00:00",
   },
   {
     score: { Chelsea: 1, "Tottenham Hotspur": 1 },
-    date: "2021-05-04T14:00:00"
+    date: "2021-05-04T14:00:00",
   },
   {
     score: { Chelsea: 1, "Leicester City": 4 },
-    date: "2021-05-03T14:00:00"
+    date: "2021-05-03T14:00:00",
   },
   {
     score: { "Tottenham Hotspur": null, "Leicester City": null },
-    date: "2021-05-09T14:00:00"
-  }
+    date: "2021-05-09T14:00:00",
+  },
 ];
 
 type gamePlayed = {
@@ -180,8 +180,8 @@ export function getData() {
       club1.nextGames.push(curr);
       club2.nextGames.push(curr);
     } else if (isGamePlayed(curr, clubKey1)) {
-      const score1 = curr.score[clubKey1] as number
-      const score2 = curr.score[clubKey2] as number
+      const score1 = curr.score[clubKey1] as number;
+      const score2 = curr.score[clubKey2] as number;
       if (score1 > score2) {
         club1.win = club1.win + 1;
         club2.loss = club2.loss + 1;
